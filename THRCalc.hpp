7f36f12aa58f608cf36ae39ddfb68e595be739ae
izzybogart <60237226+izzybogart@users.noreply.github.com>
2021-02-23 11:02:06 -0700

    Update THRCalc.hpp

--- THRCalc.hpp
+++ THRCalc.hpp
@@ -2,34 +2,33 @@
 using namespace std;
 
 class HeartRates {
-	private:
-		string firstName;
-		string lastName;
-		int dayOFbirth;
-		int monthOFbirth;
-		int yearOFbirth;
-	public:
-		//constructors
-		HeartRates();
-		HeartRates(string first_name, string last_name, int day, int month, int year);
-		void FirstName(string first_name);
-		void LastName(string last_name);
-		void DOB(int day);
-		void MOB(int month);
-		void YOB(int year);
-		
-		//get functions
-		string get_FirstName() const;
-		string get_LastName() const;
-		int get_day() const;
-		int get_month() const;
-		int get_year() const;
+        private:
+                string firstName;
+                string lastName;
+                int dayOFbirth;
+                int monthOFbirth;
+                int yearOFbirth;
+        public:
+                //constructors and destructors
+                HeartRates();
+                HeartRates(string first_name, string last_name, int day, int month, int year);
+                void FirstName(string first_name);
+                void LastName(string last_name);
+                void DOB(int day);
+                void MOB(int month);
+                void YOB(int year);
 
-		//functions
-		int getAge();
-		int getMaximumHeartRate(int);
-		void getTargetHeartRate(int);
-		void displayData(int) const;
+                //get functions
+                string get_FirstName() const;
+                string get_LastName() const;
+                int get_day() const;
+                int get_month() const;
+                int get_year() const;
+
+                //functions
+                int getAge();
+                int getMaximumHeartRate(int);
+                void getTargetHeartRate(int);
+                void displayData(int) const;
 };
-
 
